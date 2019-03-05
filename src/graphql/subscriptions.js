ca739ea73d1a1e7d69a70215b64a3e13e710f8b6
sangwoo89118 <sangwoo89118@gmail.com>
2019-03-04 23:03:14 -0800

    installed initial aws amplify and react-navigation

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,30 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    name
+    speakerName
+    spaekerBio
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    name
+    speakerName
+    spaekerBio
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    name
+    speakerName
+    spaekerBio
+  }
+}
+`;
